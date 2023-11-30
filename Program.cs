3bd1a1a25b4bd64c987a18fbcbbbc9c734bcaca4
Davit Danielyan <davit-danielyan@hotmail.com>
2023-11-30 08:19:51 +0100

    Learning c#

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MySuperBank
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var account = new BankAccount("Dav", 10000);
+      Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+      account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+      Console.WriteLine(account.Balance);
+
+      account.MakeWithdrawal(100, DateTime.Now, "PS%");
+      Console.WriteLine(account.Balance);
+
+      account.MakeWithdrawal(200, DateTime.Now, "Iphone");
+      Console.WriteLine(account.Balance);
+      
+      
+      // Test for a negative balance.
+      try
+      {
+        account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+      }
+      catch (InvalidOperationException e)
+      {
+        Console.WriteLine("Exception caught trying to overdraw");
+        Console.WriteLine(e.ToString());
+      }
+
+      try
+      {
+        var invalidAccount = new BankAccount("invalid", -55);
+      }
+      catch (ArgumentOutOfRangeException e)
+      {
+        Console.WriteLine("Exception caught creating account with negative balance");
+        Console.WriteLine(e.ToString());
+      }
+
+      account.MakeWithdrawal(50, DateTime.Now, "Last");
+      Console.WriteLine(account.Balance + " Baaa");
+    Console.WriteLine(account.GetAccountHistory());
+    }
+  }
+}
